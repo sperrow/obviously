fbe1f0af37a34233a7b1488fbfcc912963d1813c
Matthew Sperry <mattjsperry@gmail.com>
2025-01-24 03:27:01 -0800

    ignore eslint errors

--- app/library/table.tsx
+++ app/library/table.tsx
@@ -68,7 +68,9 @@ const columns = [
 ];
 
 export default function LibraryTable() {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [data, _setData] = useState(() => [...defaultData]);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [rowSelection, setRowSelection] = useState({});
 
     const table = useReactTable({
